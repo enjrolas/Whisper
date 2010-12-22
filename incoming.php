802c1ba8187cc78d15ee51b31a1989893c3ead8f
alex <japhy@li64-162.members.linode.com>
2010-12-22 10:20:54 -0500

    added intro for new callers

--- incoming.php
+++ incoming.php
@@ -8,7 +8,7 @@ if(isset($_REQUEST['Digits']))  //the user made a choice
   {
     $digits=$_REQUEST['Digits'];
     if($digits=='1'){
-      echo "<Say>Record a note for yourself</Say>";
+      echo "<Say>Record a note. Hang up when you're done</Say>";
       echo "<Record transcribe='true' transcribeCallback='transcription.php'/>";
     }
     else if($digits=='2')
@@ -34,7 +34,8 @@ date_format(timestamp, '%W the %D') as 'formattedDate'
 	    $query=$query . "\n here's a line:  $date $url";
 	    $query=$query . "\n $printedRow";
 	  }
-	
+	echo "<Say>That's all your notes</Say>
+	<Redirect>".$_SERVER['PHP_SELF']."?repeat=1</Redirect>";
       }
     else{
       echo "<Gather action='verify.php?' numDigits='4'>";
@@ -47,10 +48,28 @@ date_format(timestamp, '%W the %D') as 'formattedDate'
 else
   {
     echo "<Gather action='".$_SERVER['PHP_SELF']."' numDigits='1'>";
+    if(!isset($_REQUEST['repeat'])) //this is the first time we've run the script
+      {
+      echo "<Say>Welcome to Whisper</Say>";
+	$phoneNumber=$_REQUEST['From'];
+	$query="select * from messages where phoneNumber='$phoneNumber'";
+	$result=mysql_query($query);
+	if(mysql_num_rows($result)==0)
+	  {
+	    playIntro();
+	  }
 
-echo "<Say>To leave a note, press 1.  To hear your notes, press 2</Say>
-</Gather>";
+      }
+    echo "<Say>To leave a note, press 1.  To hear your notes, press 2</Say>
+          </Gather>";
   }
+
 echo "</Response> ";
+
+function playIntro()
+{
+  echo "<Say>Whisper is a fast, simple scratch pad for voice notes.  You can record notes for yourself, or listen to notes you've saved for yourself.  
+Your notes are tied to your phone number, so only you can access them.</Say>";
+}
 
 ?>
